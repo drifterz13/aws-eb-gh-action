ee279cc5082766da3296f69b0ccd215c5db96374
drifterz13 <mixmyxxe@gmail.com>
2021-05-02 12:19:23 +0700

    trigger build

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan("short"));
 
 app.get('/', (req, res) => {
-  res.send('Hello there! 🥩🍔🍟🍿🍘🍙🍺')
+  res.send('Hello there! 🥩🍔🍟🍿🍘🍙🍺🍻')
 })
 
 app.post("/skill", async (req, res) => {
