2628ba63bb6d015dcffefeb8c311e79c5a873a66
drifterz13 <mixmyxxe@gmail.com>
2021-05-02 11:16:49 +0700

    wip

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan("short"));
 
 app.get('/', (req, res) => {
-  res.send('Hello there! 🥩🍔🍟🍿')
+  res.send('Hello there! 🥩🍔🍟🍿🍘')
 })
 
 app.post("/skill", async (req, res) => {
